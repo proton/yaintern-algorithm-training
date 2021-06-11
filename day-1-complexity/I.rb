f8a0fbf7d75ac914034fc35366f67814e589772f
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-11 19:05:01 +0300

    simplified task 1 - I

--- day-1-complexity/I.rb
+++ day-1-complexity/I.rb
@@ -1,9 +1,9 @@
 a, b, c, d, e = 5.times.map { gets.to_i }
 
-m1 = [a, b, c].min
-m2 = a + b + c - m1 - [a, b, c].max
+d, e = [d, e].sort
+a, b, c = [a, b, c].sort
 
-if [d, e].min >= m1 && [d, e].max >= m2
+if d >= a && e >= b
   puts 'YES'
 else
   puts 'NO'
