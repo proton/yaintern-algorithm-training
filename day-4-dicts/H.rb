70cc72de8287bd119426ca9a70bafd700a5b03c2
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-13 02:40:50 +0300

    hw 4 - H - time limit

--- day-4-dicts/H.rb
+++ day-4-dicts/H.rb
@@ -1,41 +1,43 @@
-#gets
-#w = gets.strip
-#s = gets.strip
+class Counter < Hash
+  def initialize
+    super(0)
+  end
+  
+  def inc(key)
+    self[key] += 1
+  end
+  
+  def dec(key)
+    self[key] -= 1
+    delete(key) if self[key] == 0
+  end
+end
 
-#permutations = w.chars.permutation.map(&:join).uniq
-#puts permutations.map { |m| s.scan(m).size }.reduce(0) { |a,b| a + b }
+def counter(word)
+  h = Counter.new
+  word.chars.each { |c| h[c] += 1 }
+  h
+end
 
-from collections import Counter
+def decrypt(word, letter_sequence)
+  answer = 0
+  return answer if word.size < letter_sequence.size
+  
+  letter_sequence_counter = counter(letter_sequence)
+  word_counter = counter(word[0...(letter_sequence.size - 1)])
+  
+  ((letter_sequence.size - 1)...word.size).each do |index|
+    # повыссить счетчик текущего
+    word_counter.inc(word[index])
+    answer += 1 if word_counter == letter_sequence_counter
+    word_counter.dec(word[index - letter_sequence.size + 1])
+  end
+  
+  return answer
+end
 
+gets
+letter_sequence = gets.strip
+word = gets.strip
 
-def decrypt(word, letter_sequence):
-    answer = 0
-    word_length, letter_sequence_length = len(word), len(letter_sequence)
-    if word_length < letter_sequence_length:
-        return answer
-    letter_sequence_counter = Counter(letter_sequence)
-    word_counter = Counter(word[:letter_sequence_length-1])
-    index = 0
-    for index in range(letter_sequence_length - 1,
-                       word_length):
-        # повыссить счетчик текущего
-        word_counter[word[index]] += 1
-        if word_counter == letter_sequence_counter:
-            answer += 1
-        # понизить счетчик пред элемента
-        word_counter[word[index - letter_sequence_length + 1]] -= 1
-        if word_counter[word[index - letter_sequence_length + 1]] == 0:
-            # если элемента нет, то удалить из каунтера
-            del word_counter[word[index - letter_sequence_length + 1]]
-    return answer
-
-
-with open('input.txt') as file:
-    lines = file.readlines()
-
-letter_sequence = lines[1].strip()
-word = lines[2].strip()
-
-
-with open('output.txt', 'w') as file:
-    file.write(str(decrypt(word, letter_sequence)))
+puts decrypt(word, letter_sequence)
