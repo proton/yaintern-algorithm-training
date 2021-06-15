aa7ea0fcda53e75eb3b7a3d82200acb9e81516b0
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-15 19:44:46 +0300

    hw 5 - B - speed up

--- day-5-prefix-sums/B.rb
+++ day-5-prefix-sums/B.rb
@@ -1,21 +1,32 @@
 def count_cars(cars, k)
+  t = Time.now
   cnt = 0
 
+  prev_sum = nil
+  prev_j = nil
   cars.each_index do |i|
     j = i
     sum = 0
-    els = []
-    while i < cars.size && j < cars.size
+    #els = []
+    loop do
       sum += cars[j]
-      els << j
+      #els << j
       if sum == k
         cnt += 1
         #p els
+        prev_sum = sum
+        prev_j = j
         break
       elsif sum > k
+        prev_sum = sum
+        prev_j = j
         break
       end
       j += 1
+      if j == cars.size
+        return cnt
+        break
+      end
     end
   end
 
