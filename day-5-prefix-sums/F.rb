26df1effc8fc091274b587a185a71fb5ad1ea72b
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-16 15:24:08 +0300

    started HW 5 - F

--- day-5-prefix-sums/F.rb
+++ day-5-prefix-sums/F.rb
@@ -4,8 +4,14 @@ lines = File.open('input.txt').readlines
 n = lines[0].to_i
 powers = lines[1].split.map(&:to_i)
 m = lines[2].to_i
-conditioners = lines[3..(3 + m)].map { |l| l.split.map(&:to_i) }.sort
+conditioners = lines[3..(3 + m)].map { |l| l.split.map(&:to_i).reverse }.sort
 
-powers.map do |power|
-  #
-end
+p conditioners
+
+r = powers.map do |power|
+  conditioner = conditioners.bsearch { |(cost, c_power)| c_power >= power }
+  p [power, conditioner]
+  conditioner[0] # cost
+end.sum
+
+puts r
