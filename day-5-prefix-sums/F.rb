3bbcf06d4954d38bc7e1cc396cdbe4a65a1854a6
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-16 18:51:23 +0300

    hw 5 - F

--- day-5-prefix-sums/F.rb
+++ day-5-prefix-sums/F.rb
@@ -1,17 +1,26 @@
-
+def calculate_power(powers, conditioners)
+  cost = 0
+  j = 0
+  powers.each do |power|
+    loop do
+      conditioner_price, conditioner_power = conditioners[j]
+      if conditioner_power >= power
+        cost += conditioner_price
+        break
+      else
+        j += 1
+      end
+    end
+  end
+  cost
+end
 
 lines = File.open('input.txt').readlines
 n = lines[0].to_i
-powers = lines[1].split.map(&:to_i)
+powers = lines[1].split.map(&:to_i).sort
 m = lines[2].to_i
 conditioners = lines[3..(3 + m)].map { |l| l.split.map(&:to_i).reverse }.sort
 
-p conditioners
-
-r = powers.map do |power|
-  conditioner = conditioners.bsearch { |(cost, c_power)| c_power >= power }
-  p [power, conditioner]
-  conditioner[0] # cost
-end.sum
+r = calculate_power(powers, conditioners)
 
 puts r
