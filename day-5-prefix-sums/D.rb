2f544962b36f0d002f128f4b53be62437e003889
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-16 03:20:46 +0300

    hw 5 - D - time limit on 38

---
+++ day-5-prefix-sums/D.rb
@@ -0,0 +1,23 @@
+# find all combinations of two nums with diff > r
+def find_cnt(nums, r)
+  cnt = 0
+  nums.each_with_index do |num, i|
+    j = i + 1
+    while j < nums.size
+      num2 = nums[j]
+      if num2 - num > r
+        cnt += nums.size - j
+        break
+      end
+      j += 1
+    end
+  end
+  cnt
+end
+
+lines = File.open('input.txt').readlines
+n, r = lines[0].split.map(&:to_i)
+ds = lines[1].split.map(&:to_i)
+
+r = find_cnt(ds, r)
+puts r
