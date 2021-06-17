1618188ef3ac9a506ef9c389b7f21705df5b2162
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-17 21:36:04 +0300

    hw 6 - H

--- day-6-binary-search/H.rb
+++ day-6-binary-search/H.rb
@@ -16,7 +16,8 @@ n, k = lines[0].split.map(&:to_i)
 segments = lines[1..n].map(&:to_i)
 
 l = rbinsearch(0, segments.max) do |l|
-  segments.map { |s| s / l }.reduce(0, :+) >= 11
+  # p [__LINE__, l, segments.map { |s| s / l }]
+  segments.map { |s| s / l }.reduce(0, :+) >= k
 end
 
 p l
