e6a5b491e7490369d2a387339dac2fe70e54944d
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-17 18:06:27 +0300

    hw 6 - D

--- day-6-binary-search/D.rb
+++ day-6-binary-search/D.rb
@@ -13,8 +13,11 @@ end
 lines = File.open('input.txt').readlines
 n, a, b, w, h = lines[0].split.map(&:to_i)
 
-r = rbinsearch(0, 10**20) do |d|
+r1 = rbinsearch(0, 10**20) do |d|
   (w / (a + 2*d)) * (h / (b + 2*d)) >= n
 end
+r2 = rbinsearch(0, 10**20) do |d|
+  (w / (b + 2*d)) * (h / (a + 2*d)) >= n
+end
 
-p r
+p [r1, r2].max
