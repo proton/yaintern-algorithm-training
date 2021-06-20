26e50c498c5fa2303b15381707a5f7a85ad1249a
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-20 09:37:10 +0300

    removed commented lines

--- day-6-binary-search/H.rb
+++ day-6-binary-search/H.rb
@@ -1,7 +1,6 @@
 def rbinsearch(l, r, &block)
   while l < r
     m = (l + r + 1) / 2
-    # p [__LINE__, m, block[m]]
     if block[m]
       l = m
     else
@@ -16,7 +15,6 @@ n, k = lines[0].split.map(&:to_i)
 segments = lines[1..n].map(&:to_i)
 
 l = rbinsearch(0, segments.max) do |l|
-  # p [__LINE__, l, segments.map { |s| s / l }]
   segments.map { |s| s / l }.reduce(0, :+) >= k
 end
 
