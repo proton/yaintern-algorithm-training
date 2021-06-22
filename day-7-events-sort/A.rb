c8f4f80fa18cbcf7cef16b9ca313c893854202fc
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-22 06:35:27 +0300

    hw 7 - A

--- day-7-events-sort/A.rb
+++ day-7-events-sort/A.rb
@@ -1,13 +1,33 @@
 lines = File.open('input.txt').readlines
 n, m = lines[0].split.map(&:to_i)
-watched_tables = lines[1..m].map { |l| a, b = l.split.map(&:to_i).sort; (a..b).to_a }
+# watched_tables = lines[1..m].map { |l| a, b = l.split.map(&:to_i).sort; (a..b).to_a }
+# watched_tables = lines[1..m].map { |l| a, b = l.split.map(&:to_i).sort; (a..b) }.sort
 
+events = []
+lines[1..m].each do |l|
+  a, b = l.split.map(&:to_i).sort
+  events << [a, 0]
+  events << [b, 1]
+end
+events.sort!
 
-watched_students = watched_tables.flatten.uniq
+watched_students = 0
+cnt = 0
+s_start = s_end = 0
+events.each do |event|
+  d, type = event
+  if type == 0
+    cnt += 1
+    s_start = d if cnt == 1
+  else
+    cnt -= 1
+    if cnt == 0
+      s_end = d
+      watched_students += s_end - s_start + 1
+    end
+  end
+end
 
-p n - watched_students.size
-
-
-# Решившие 7 A, подскажите пожалуйста ответ в 7ом тесте (входные данные https://pastebin.com/XGjFAacy)
-
-# 1000 учеников, преподаватели смотрят на 877 уникальных парт, 1000 - 877 = 123, но боту мой ответ не нравится :(
+#watched_students = watched_tables.flatten.uniq
+# p watched_students
+p n - watched_students
