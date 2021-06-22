d3d5f80eda93927a876493513f338ef5d85d9844
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-22 05:20:27 +0300

    hw 7 - B

--- day-7-events-sort/B.rb
+++ day-7-events-sort/B.rb
@@ -1,6 +1,29 @@
 lines = File.open('input.txt').readlines
 n, m = lines[0].split.map(&:to_i)
-segments = lines[1..n].map { |l| l.split.map(&:to_i) }
+segments = lines[1..n].map { |l| l.split.map(&:to_i).sort }
 dots = lines[n + 1].split.map(&:to_i)
 
-puts dots.map { |d| segments.count { |s| s.first <= d && s.last >= d } }.join(' ')
+events = []
+segments.each do |s|
+  events << [s.first, 0]
+  events << [s.last, 2]
+end
+dots.each_with_index do |d, i|
+  events << [d, 1, i]
+end
+events.sort!
+
+r = []
+cnt = 0
+events.each do |event|
+  if event[1] == 0
+    cnt += 1
+  elsif event[1] == 2
+    cnt -= 1
+  else
+    i = event.last
+    r[i] = cnt
+  end
+end
+
+puts r.join(' ')
