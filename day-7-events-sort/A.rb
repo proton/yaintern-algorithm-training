13beac43c2c667e80f483b60f120a2be5dd36c58
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-22 05:39:58 +0300

    hw 7 - A - memory limit on test 8

--- day-7-events-sort/A.rb
+++ day-7-events-sort/A.rb
@@ -1,6 +1,13 @@
 lines = File.open('input.txt').readlines
 n, m = lines[0].split.map(&:to_i)
-watched_tables = lines[1..m].map { |l| l.split.map(&:to_i) }
+watched_tables = lines[1..m].map { |l| a, b = l.split.map(&:to_i).sort; (a..b).to_a }
+
+
 watched_students = watched_tables.flatten.uniq
 
 p n - watched_students.size
+
+
+# Решившие 7 A, подскажите пожалуйста ответ в 7ом тесте (входные данные https://pastebin.com/XGjFAacy)
+
+# 1000 учеников, преподаватели смотрят на 877 уникальных парт, 1000 - 877 = 123, но боту мой ответ не нравится :(
