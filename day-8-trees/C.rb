dcc13504828244e1f2460f379a3f06fd9fa79583
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-23 22:24:47 +0300

    hw 8 - C

--- day-8-trees/C.rb
+++ day-8-trees/C.rb
@@ -1,17 +1,37 @@
-TreeNode = Struct.new(:parent, :left, :right, :value) do
-  def insert(node)
+class Tree
+  attr_accessor :parent, :left, :right, :value
+
+  def initialize(value: nil)
+    @value = value
+  end
+
+  def empty?
+    value.nil?
+  end
+
+  def insert(value)
+    if empty?
+      self.value = value
+      return
+    end
+
+    node = Tree.new(value: value)
+    insert_node(node)
+  end
+
+  def insert_node(node)
     if node.value == value
       # do nothing
     elsif node.value > value
       if right
-        right.insert(node)
+        right.insert_node(node)
       else
         self.right = node
         node.parent = self
       end
     else
       if left
-        left.insert(node)
+        left.insert_node(node)
       else
         self.left = node
         node.parent = self
@@ -20,41 +40,44 @@ TreeNode = Struct.new(:parent, :left, :right, :value) do
   end
 
   def height
+    return 0 if empty?
+
     1 + [(left ? left.height : 0), (right ? right.height : 0)].compact.max
   end
 
   def greatest
+    return if empty?
+
     right ? right.greatest : value
-  end
-end
-
-class Tree
-  attr_reader :root
-
-  def insert(value)
-    node = TreeNode.new
-    node.value = value
-    if root
-      root.insert(node)
-    else
-      @root = node
-    end
-  end
-
-  def height
-    root ? root.height : 0
-  end
-
-  def greatest
-    root.greatest if root
   end
 
   def second_greatest
-    return if root.nil?
+    return if empty?
 
-    node = root
+    node = self
     node = node.right while node.right
-    node.left ? node.left.value : node.parent.value
+
+    if node.left
+      node.left.greatest
+    elsif node.parent
+      node.parent.value
+    end
+  end
+
+  def sorted_values
+    r = []
+    r += left.sorted_values if left
+    r << self.value
+    r += right.sorted_values if right
+    r
+  end
+
+  def sorted_nodes
+    r = []
+    r += left.sorted_nodes if left
+    r << self
+    r += right.sorted_nodes if right
+    r
   end
 end
 
