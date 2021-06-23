0aae03d6e554ffd93ef1ff12228a5ff03c199550
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-24 00:03:42 +0300

    hw 8 - I

--- day-8-trees/I.rb
+++ day-8-trees/I.rb
@@ -1,17 +1,13 @@
 class Tree
-  attr_accessor :children, :value
+  attr_accessor :children, :value, :parent
 
   def initialize(value: nil)
     @value = value
     @children = []
   end
 
-  def empty?
-    value.nil?
-  end
-
   def sub_children_count
-    children.size + children.map(&:sub_children_count).reduce(0, :+)
+    @sub_children_count ||= children.size + children.map(&:sub_children_count).reduce(0, :+)
   end
 end
 
@@ -27,6 +23,13 @@ raw.each do |(child, parent)|
   h[child] ||= Tree.new(value: child)
   child_node = h[child]
   parent_node.children << child_node
+  child_node.parent = parent_node
+end
+
+nodes = h.values.select { |node| node.children.empty? }
+while !nodes.empty?
+  nodes.each(&:sub_children_count)
+  nodes = nodes.map(&:parent).uniq.compact
 end
 
 h.keys.sort.each do |key|
